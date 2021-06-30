7757bfedeeae6e50c72eeb335d65a66611e3ea8c
rmsf-zjh <zhuok520@qq.com>
2021-06-30 23:34:22 +0800

    ADD

---
+++ src/lib/TopExpressConfigManage.php
@@ -0,0 +1,86 @@
+<?php
+
+
+namespace RmTop\RmExpress\lib;
+
+
+use RmTop\RmExpress\model\TopExpressConfig;
+
+class TopExpressConfigManage
+{
+
+
+    /**
+     * 创建圆通配置
+     * @param string $expressName //快递配置名称
+     * @param string $identify  //标识符
+     * @param string $apiUrl
+     * @param string $clientId
+     * @param string $key
+     * @param string $version
+     * @return TopExpressConfig|Model
+     */
+    static function addConfig(string $expressName,string $identify, string $apiUrl,string $clientId,string $key,string $version){
+        $data['title'] = $expressName;
+        $data['identify'] = $identify;
+        $config['apiUrl'] = $apiUrl;
+        $config['version'] = $version;
+        $config['clientId'] = $clientId;
+        $config['key'] = $key;
+        $data['config'] = serialize($config);
+        return   TopExpressConfig::create($data);
+    }
+
+
+    /**
+     * 修改配置
+     * @param string $expressName
+     * @param string $identify
+     * @param string $apiUrl
+     * @param int $id
+     * @param string $clientId
+     * @param string $key
+     * @param string $version
+     * @return bool
+     */
+    static  function editConfig(int $id,string $expressName,string $identify, string $apiUrl,string $clientId,string $key,string $version): bool
+    {
+        $find =  TopExpressConfig::find($id);
+        $find->config = serialize([
+            'apiUrl'=>$apiUrl,
+            'clientId'=>$clientId,
+            'key'=>$key,
+            'version'=>$version
+        ]);
+        $find->title = $expressName;
+        $find->identify = $identify;
+        return $find->save();
+    }
+
+
+    /**
+     * 返回配置项
+     * @param int $id
+     * @return mixed
+     * @throws DataNotFoundException
+     * @throws DbException
+     * @throws ModelNotFoundException
+     */
+    static   function getConfig(int $id){
+        $find =  TopExpressConfig::find($id);
+        return unserialize($find['config']);
+    }
+
+
+    /**删除配置项
+     * @param int $id
+     * @return bool
+     */
+    static  function deleteConfig(int $id): bool
+    {
+        return TopExpressConfig::where(['id'=>$id])->delete();
+    }
+
+// ---------------------------------------------- 圆通数据处理  ---------------------------------------
+
+}
